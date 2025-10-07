5c59c13402c839d8fd39cc61bfd789e3af1775e5
Ishanvi1486 <ishanviagarwal54@gmail.com>
2025-10-07 16:03:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
 # Download required resources to that folder
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('punkt', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 
@@ -69,5 +70,6 @@ if st.button('Click to Predict'):
         st.header("Spam")
     else:
         st.header('Not Spam')
+
 
 
